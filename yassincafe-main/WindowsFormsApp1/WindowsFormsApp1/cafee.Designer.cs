f2d06300f00109f621fd25852d295b0a1da73fdd
OBAM4 <65874954+OBAM4@users.noreply.github.com>
2022-08-31 18:08:42 +0200

    Add files via upload

---
+++ yassincafe-main/WindowsFormsApp1/WindowsFormsApp1/cafee.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class cafee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMod = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TimeInsert = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.txtnaf = new System.Windows.Forms.TextBox();
+            this.txtDakhl = new System.Windows.Forms.TextBox();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.btnAjou = new System.Windows.Forms.Button();
+            this.dataGridCafe = new System.Windows.Forms.DataGridView();
+            this.btntact = new System.Windows.Forms.Button();
+            this.btnSupp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCafe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMod
+            // 
+            this.btnMod.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnMod.Location = new System.Drawing.Point(28, 105);
+            this.btnMod.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMod.Name = "btnMod";
+            this.btnMod.Size = new System.Drawing.Size(100, 23);
+            this.btnMod.TabIndex = 62;
+            this.btnMod.Text = "Modifier";
+            this.btnMod.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(773, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 60;
+            this.label2.Text = "النفقات";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(773, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 59;
+            this.label1.Text = "الدخل";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(561, 37);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(13, 308);
+            this.panel1.TabIndex = 58;
+            // 
+            // TimeInsert
+            // 
+            this.TimeInsert.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TimeInsert.Location = new System.Drawing.Point(743, 73);
+            this.TimeInsert.Margin = new System.Windows.Forms.Padding(4);
+            this.TimeInsert.Name = "TimeInsert";
+            this.TimeInsert.Size = new System.Drawing.Size(268, 22);
+            this.TimeInsert.TabIndex = 55;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dateTimePicker2.Location = new System.Drawing.Point(171, 197);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker2.TabIndex = 56;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dateTimePicker1.Location = new System.Drawing.Point(171, 107);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 57;
+            // 
+            // txttotal
+            // 
+            this.txttotal.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txttotal.Location = new System.Drawing.Point(501, 711);
+            this.txttotal.Margin = new System.Windows.Forms.Padding(4);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(151, 22);
+            this.txttotal.TabIndex = 52;
+            // 
+            // txtnaf
+            // 
+            this.txtnaf.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtnaf.Location = new System.Drawing.Point(855, 148);
+            this.txtnaf.Margin = new System.Windows.Forms.Padding(4);
+            this.txtnaf.Name = "txtnaf";
+            this.txtnaf.Size = new System.Drawing.Size(132, 22);
+            this.txtnaf.TabIndex = 53;
+            // 
+            // txtDakhl
+            // 
+            this.txtDakhl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtDakhl.Location = new System.Drawing.Point(855, 105);
+            this.txtDakhl.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDakhl.Name = "txtDakhl";
+            this.txtDakhl.Size = new System.Drawing.Size(132, 22);
+            this.txtDakhl.TabIndex = 54;
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnTotal.Location = new System.Drawing.Point(361, 705);
+            this.btnTotal.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(100, 34);
+            this.btnTotal.TabIndex = 50;
+            this.btnTotal.Text = "Total ";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            // 
+            // btnAjou
+            // 
+            this.btnAjou.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAjou.Location = new System.Drawing.Point(855, 202);
+            this.btnAjou.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAjou.Name = "btnAjou";
+            this.btnAjou.Size = new System.Drawing.Size(100, 28);
+            this.btnAjou.TabIndex = 51;
+            this.btnAjou.Text = "Ajouter";
+            this.btnAjou.UseVisualStyleBackColor = true;
+            this.btnAjou.Click += new System.EventHandler(this.btnAjou_Click);
+            // 
+            // dataGridCafe
+            // 
+            this.dataGridCafe.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridCafe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridCafe.Location = new System.Drawing.Point(1, 350);
+            this.dataGridCafe.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridCafe.Name = "dataGridCafe";
+            this.dataGridCafe.Size = new System.Drawing.Size(1104, 348);
+            this.dataGridCafe.TabIndex = 49;
+            this.dataGridCafe.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridCafe_CellContentClick);
+            // 
+            // btntact
+            // 
+            this.btntact.Location = new System.Drawing.Point(724, 310);
+            this.btntact.Name = "btntact";
+            this.btntact.Size = new System.Drawing.Size(75, 23);
+            this.btntact.TabIndex = 63;
+            this.btntact.Text = "actualiser";
+            this.btntact.UseVisualStyleBackColor = true;
+            this.btntact.Click += new System.EventHandler(this.btntact_Click);
+            // 
+            // btnSupp
+            // 
+            this.btnSupp.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSupp.Location = new System.Drawing.Point(28, 197);
+            this.btnSupp.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSupp.Name = "btnSupp";
+            this.btnSupp.Size = new System.Drawing.Size(135, 57);
+            this.btnSupp.TabIndex = 61;
+            this.btnSupp.Text = "supprimer";
+            this.btnSupp.UseVisualStyleBackColor = true;
+            // 
+            // cafee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 775);
+            this.Controls.Add(this.btntact);
+            this.Controls.Add(this.btnSupp);
+            this.Controls.Add(this.btnMod);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TimeInsert);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.txtnaf);
+            this.Controls.Add(this.txtDakhl);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnAjou);
+            this.Controls.Add(this.dataGridCafe);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "cafee";
+            this.Text = "cafee";
+            this.Load += new System.EventHandler(this.cafee_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCafe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSupp;
+        private System.Windows.Forms.Button btnMod;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker TimeInsert;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.TextBox txtnaf;
+        private System.Windows.Forms.TextBox txtDakhl;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnAjou;
+        private System.Windows.Forms.DataGridView dataGridCafe;
+        private System.Windows.Forms.Button btntact;
+    }
+}
---
+++ yassincafe-main/WindowsFormsApp1/WindowsFormsApp1/cafee.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class cafee : Form
+    {
+        public cafee()
+        {
+            InitializeComponent();
+        }
+        cnx c = new cnx();
+        public void act()
+        {
+            c.connecter();
+            c.cmd.CommandText = " select * from cafe ";
+            c.cmd.Connection = c.cn;
+            c.dr = c.cmd.ExecuteReader();
+            c.dt.Load(c.dr);
+            dataGridCafe.DataSource = c.dt;
+            c.deconnecter();
+        }
+        public void Ajoutercolonne()
+        {
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            btn.Name = " Suppression ";
+            btn.Text = " Supprimer ";
+            btn.UseColumnTextForButtonValue = true;
+            dataGridCafe.Columns.Add(btn);
+        }
+        
+
+        private void cafee_Load(object sender, EventArgs e)
+        {
+            act();
+            Ajoutercolonne();
+        }
+
+        private void btntact_Click(object sender, EventArgs e)
+        {
+            c.dt.Clear();
+            act();
+        }
+
+        private void btnAjou_Click(object sender, EventArgs e)
+        {
+          
+                c.connecter();
+                c.cmd.CommandText = "insert into [cafe] values ('"+txtDakhl.Text+"','"+txtnaf.Text+"','"+ TimeInsert.Value + "')";
+                c.cmd.Connection = c.cn;
+                c.cmd.ExecuteNonQuery();
+                MessageBox.Show("Lugne affecté");
+                c.deconnecter();
+                c.dt.Clear();
+                act();
+          
+        }
+
+        private void dataGridCafe_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex!=3)
+            {
+                return;
+            }
+            if(MessageBox.Show("?تريد المسح ، هل أنت متأكد","Message",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                
+            }
+        }
+    }
+}
